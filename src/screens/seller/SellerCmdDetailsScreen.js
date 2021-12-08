2c735ff7a2551ef8d3d8e64f8a4e2680123ddff9
Yassiremt <yassiremtioui@gmail.com>
2021-12-08 20:06:30 +0100

    the address of customer in small characters

--- src/screens/seller/SellerCmdDetailsScreen.js
+++ src/screens/seller/SellerCmdDetailsScreen.js
@@ -71,10 +71,9 @@ const SellerCmdDetailsScreen = ({ navigation, route }) => {
           {"ID de commande"}
         </Text>
         <Text style={[styles.value, { color: "#616161" }]}>{_id}</Text>
-        <Text style={[styles.key, { color: colors.black }]}>Nom de client</Text>
+        <Text style={[styles.key, { color: colors.black }]}>Client</Text>
         <Text style={[styles.value, { color: "#616161" }]}>{clientName}</Text>
-        <Text style={[styles.key, { color: colors.black }]}>Son address</Text>
-        <Text style={[styles.value, { color: "#616161" }]}>
+        <Text style={[styles.address, { color: "#616161" }]}>
           {clientAddress}
         </Text>
         <Text style={[styles.key, { color: colors.black }]}>
@@ -191,6 +190,12 @@ const styles = StyleSheet.create({
     fontSize: 22,
     marginBottom: 10,
     lineHeight: 30
+  },
+  address: {
+    fontFamily: "Montserrat-Medium",
+    fontSize: 15,
+    marginBottom: 10,
+    lineHeight: 22
   },
   scrollStyle: {
     paddingHorizontal: 25,
