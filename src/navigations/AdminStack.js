489608166e32083207782a18f132aac3a40baf36
Yassiremt <yassiremtioui@gmail.com>
2021-12-11 04:38:46 +0100

    deleted unused cmds screen

--- src/navigations/AdminStack.js
+++ src/navigations/AdminStack.js
@@ -4,7 +4,6 @@ import AdminHomeScreen from "../screens/admin/AdminHomeScreen";
 import CommercesScreen from "../screens/admin/CommercesScreen";
 import PersonnelsScreen from "../screens/admin/PersonnelsScreen";
 import ProduitsScreen from "../screens/admin/ProduitsScreen";
-import CommandesScreen from "../screens/admin/CommandesScreen";
 import CommerceProduitsScreen from "../screens/admin/CommerceProduitsScreen";
 import CommandeDetailsScreen from "../screens/admin/CommandeDetailsScreen";
 import StatistiquesScreen from "../screens/admin/StatistiquesScreen";
@@ -30,7 +29,6 @@ const MainStack = ({}) => (
     <Stack.Screen name="Commerces" component={CommercesScreen} />
     <Stack.Screen name="Personnels" component={PersonnelsScreen} />
     <Stack.Screen name="CommerceProduits" component={CommerceProduitsScreen} />
-    <Stack.Screen name="Commandes" component={CommandesScreen} />
     <Stack.Screen name="Produits" component={ProduitsScreen} />
     <Stack.Screen name="Statistiques" component={StatistiquesScreen} />
     <Stack.Screen name="CommandeDetails" component={CommandeDetailsScreen} />
