00000ce293ccab09508b751da94b2f6175a47dd9
Yassiremt <yassiremtioui@gmail.com>
2021-12-17 02:40:40 +0100

    fixed the input that doesn't show

--- src/screens/seller/AddOrderScreen.js
+++ src/screens/seller/AddOrderScreen.js
@@ -5,7 +5,9 @@ import {
   StyleSheet,
   ScrollView,
   Modal,
-  Pressable
+  Pressable,
+  KeyboardAvoidingView,
+  Platform
 } from "react-native";
 import { useTheme } from "@react-navigation/native";
 import {
@@ -92,132 +94,138 @@ const AddOrderScreen = ({ navigation, route }) => {
           setProducts={val => setProducts(val)}
         />
       </Modal>
-      <ScrollView
-        overScrollMode={"never"}
-        contentContainerStyle={styles.scrollStyle}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
       >
-        <TopBar />
-        <Label>{"Ajouter une\ncommande"}</Label>
-        <View marginVertical={20} />
-        <View alignItems={"center"}>
-          <Selector
-            label="Livreur"
-            text={selectedDelivery?.fullName || "Choisir un livreur"}
-            onPress={() => setDeliveriesModal(true)}
-          />
-          <Input
-            label="Nom de client"
-            placeholder="Nom de client"
-            value={clientName}
-            onChangeText={val => setClientName(val)}
-          />
-          {clientPhones.map((p, index1) => (
-            <Input
-              key={index1}
-              label={`Téléphone (${index1 + 1})`}
-              placeholder="01-23-45-56-78"
-              value={p}
-              onChangeText={val =>
-                setClientPhones([
-                  ...clientPhones.map((phone, index2) =>
-                    index1 === index2 ? val : phone
-                  )
-                ])
-              }
-              keyboardType={"decimal-pad"}
+        <ScrollView
+          overScrollMode={"never"}
+          contentContainerStyle={styles.scrollStyle}
+        >
+          <TopBar />
+          <Label>{"Ajouter une\ncommande"}</Label>
+          <View marginVertical={20} />
+          <View alignItems={"center"}>
+            <Selector
+              label="Livreur"
+              text={selectedDelivery?.fullName || "Choisir un livreur"}
+              onPress={() => setDeliveriesModal(true)}
             />
-          ))}
-          <View flexDirection={"row"}>
-            <Pressable
-              style={({ pressed }) => [
-                { opacity: pressed ? 0.7 : 1, marginHorizontal: 10 }
-              ]}
-              onPress={() => setClientPhones([...clientPhones, ""])}
-            >
-              <Text style={[styles.btn, { color: colors.green }]}>
-                AJOUTER PLUS
-              </Text>
-            </Pressable>
-            {clientPhones.length > 1 && (
+            <Input
+              label="Nom de client"
+              placeholder="Nom de client"
+              value={clientName}
+              onChangeText={val => setClientName(val)}
+            />
+            {clientPhones.map((p, index1) => (
+              <Input
+                key={index1}
+                label={`Téléphone (${index1 + 1})`}
+                placeholder="01-23-45-56-78"
+                value={p}
+                onChangeText={val =>
+                  setClientPhones([
+                    ...clientPhones.map((phone, index2) =>
+                      index1 === index2 ? val : phone
+                    )
+                  ])
+                }
+                keyboardType={"decimal-pad"}
+              />
+            ))}
+            <View flexDirection={"row"}>
               <Pressable
                 style={({ pressed }) => [
                   { opacity: pressed ? 0.7 : 1, marginHorizontal: 10 }
                 ]}
-                onPress={() => setClientPhones([...clientPhones.slice(0, -1)])}
+                onPress={() => setClientPhones([...clientPhones, ""])}
               >
-                <Text style={[styles.btn, { color: colors.red }]}>
-                  SUPPRIMER
+                <Text style={[styles.btn, { color: colors.green }]}>
+                  AJOUTER PLUS
                 </Text>
               </Pressable>
-            )}
+              {clientPhones.length > 1 && (
+                <Pressable
+                  style={({ pressed }) => [
+                    { opacity: pressed ? 0.7 : 1, marginHorizontal: 10 }
+                  ]}
+                  onPress={() =>
+                    setClientPhones([...clientPhones.slice(0, -1)])
+                  }
+                >
+                  <Text style={[styles.btn, { color: colors.red }]}>
+                    SUPPRIMER
+                  </Text>
+                </Pressable>
+              )}
+            </View>
+            <Input
+              label="Adresse de client"
+              placeholder="Adresse de client"
+              value={clientAddress}
+              onChangeText={val => setClientAddress(val)}
+              multiline
+            />
+            <Selector
+              label="Date de livraison"
+              text={dayjs(deliveryDate).format("YYYY-MM-DD")}
+              onPress={() => setDateModal(true)}
+            />
+            <Selector
+              label="Ajouter des produits"
+              text={
+                products.length > 0
+                  ? products.reduce((acc, p) => acc + p.name + " ", "")
+                  : "Ajouter des produits"
+              }
+              onPress={() => setProductsSelectionModal(true)}
+            />
+            <Input
+              label="Comments"
+              placeholder="Comments"
+              value={comments}
+              onChangeText={val => setComments(val)}
+              multiline
+            />
           </View>
-          <Input
-            label="Adresse de client"
-            placeholder="Adresse de client"
-            value={clientAddress}
-            onChangeText={val => setClientAddress(val)}
-            multiline
-          />
-          <Selector
-            label="Date de livraison"
-            text={dayjs(deliveryDate).format("YYYY-MM-DD")}
-            onPress={() => setDateModal(true)}
-          />
-          <Selector
-            label="Ajouter des produits"
-            text={
-              products.length > 0
-                ? products.reduce((acc, p) => acc + p.name + " ", "")
-                : "Ajouter des produits"
-            }
-            onPress={() => setProductsSelectionModal(true)}
-          />
-          <Input
-            label="Comments"
-            placeholder="Comments"
-            value={comments}
-            onChangeText={val => setComments(val)}
-            multiline
-          />
-        </View>
-        <View marginVertical={20} />
-        <Button
-          onPress={() => {
-            if (order?.clientName) {
-              dispatch(
-                updateOrder({
-                  orderId: order?._id,
-                  deliveryDate,
-                  clientAddress,
-                  clientPhones,
-                  clientName,
-                  delivery: selectedDelivery._id,
-                  productsDetails: products,
-                  comments,
-                  oldDelivery: order?.delivery._id,
-                  toBeDeletedProducts: order?.oldProducts,
-                  oldProductsIds: order?.products.map(o => o._id)
-                })
-              );
-            } else {
-              dispatch(
-                createOrder({
-                  deliveryDate,
-                  clientAddress,
-                  clientPhones,
-                  clientName,
-                  delivery: selectedDelivery._id,
-                  productsDetails: products,
-                  comments
-                })
-              );
-            }
-          }}
-        >
-          {order?.clientName ? "Mettre à jour" : "Ajouter"}
-        </Button>
-        <View marginVertical={20} />
-      </ScrollView>
+          <View marginVertical={20} />
+          <Button
+            onPress={() => {
+              if (order?.clientName) {
+                dispatch(
+                  updateOrder({
+                    orderId: order?._id,
+                    deliveryDate,
+                    clientAddress,
+                    clientPhones,
+                    clientName,
+                    delivery: selectedDelivery._id,
+                    productsDetails: products,
+                    comments,
+                    oldDelivery: order?.delivery._id,
+                    toBeDeletedProducts: order?.oldProducts,
+                    oldProductsIds: order?.products.map(o => o._id)
+                  })
+                );
+              } else {
+                dispatch(
+                  createOrder({
+                    deliveryDate,
+                    clientAddress,
+                    clientPhones,
+                    clientName,
+                    delivery: selectedDelivery._id,
+                    productsDetails: products,
+                    comments
+                  })
+                );
+              }
+            }}
+          >
+            {order?.clientName ? "Mettre à jour" : "Ajouter"}
+          </Button>
+          <View marginVertical={20} />
+        </ScrollView>
+      </KeyboardAvoidingView>
     </Container>
   );
 };
